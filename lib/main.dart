c565ce463f141e849c5a6455dd3ce204953ae536
Felipe Silva <felipemcs@dcomp.ufs.br>
2022-11-09 13:10:37 +0000

    Add Transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_app/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,14 +8,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+  ];
+  
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
